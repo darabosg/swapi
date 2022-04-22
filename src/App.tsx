adfb2f1a412f6248064f0adcddabed07fb2e74d3
Darabos Gábor <darabosg>
2022-04-22 09:35:35 +0200

    router try2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import {
-  HashRouter,
-  BrowserRouter,
-  Route,
-  Navigate,
-  Routes,
-} from 'react-router-dom';
+import { BrowserRouter, Route, Navigate, Routes } from 'react-router-dom';
 
 import './App.css';
 import Header from './components/Header/Header';
