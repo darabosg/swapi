89690a771457abae3c50846345288fa34ea89c8a
Darabos Gábor <darabosg>
2022-04-22 09:33:17 +0200

    router try

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import {
+  HashRouter,
+  BrowserRouter,
+  Route,
+  Navigate,
+  Routes,
+} from 'react-router-dom';
 
 import './App.css';
+import Header from './components/Header/Header';
+import Character from './pages/Character/Character';
+import Home from './pages/Home/Home';
+import Species from './pages/Species/Species';
 
 function App() {
-  return <div className="App">ASD</div>;
+  return (
+    <>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="species" element={<Species />} />
+          <Route path="character" element={<Character />} />
+          <Route path="*" element={<Navigate replace to="/" />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
